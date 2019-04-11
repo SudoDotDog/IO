3e2fc2c62892fa5dc2d72bd690e93d3c926e529b
WMXPY <wm@mengw.io>
2019-04-11 14:54:40 -0500

    1.2.1 release, bugfix, add unzip error code

--- src/compress.ts
+++ src/compress.ts
@@ -11,12 +11,20 @@ export const decompressZipFile = (filePath: string, targetPath: string) =>
 
         const unzip: ChildProcess = spawn('unzip', ['-o', filePath, '-d', targetPath]);
         unzip.on('exit', (code: number, signal: string) => {
-            if (code === 0) {
-                resolve();
-                return;
-            } else {
-                reject(new Error(signal));
-                return;
+
+            switch (code) {
+                case 0: {
+                    resolve();
+                    return;
+                }
+                case 9: {
+                    reject(new Error('the specified zipfiles were not found.'));
+                    return;
+                }
+                default: {
+                    reject(new Error(signal));
+                    return;
+                }
             }
         });
         unzip.on('error', (error: Error) => {
