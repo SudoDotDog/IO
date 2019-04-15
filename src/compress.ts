eb10ac0de15ac2b375a44e3fdc77a04a69e23f90
WMXPY <wm@mengw.io>
2019-04-15 18:50:20 -0500

    implement os switch

--- src/compress.ts
+++ src/compress.ts
@@ -5,6 +5,7 @@
  */
 
 import { ChildProcess, exec, spawn } from "child_process";
+import * as Os from "os";
 import * as Path from "path";
 
 export const dependableDecompressZipFile = (filePath: string, targetPath: string): Promise<string> =>
@@ -41,22 +42,23 @@ export const dependableCompressZipFileWindows = (folderPath: string, zipPath: st
         const parsedFolderPath: string = Path.resolve(folderPath);
         const parsedZipPath: string = Path.resolve(zipPath);
 
-        const compressArchive: ChildProcess = spawn('powershell', ['-nologo',
+        const compressArchive: ChildProcess = spawn('powershell', [
+            '-nologo',
             '-noprofile',
             '-command',
             '$progressPreference = "silentlyContinue"',
-            `Compress-Archive -Path "${parsedFolderPath}" -DestinationPath "${parsedZipPath}"`
+            `Compress-Archive -Path "${parsedFolderPath}" -DestinationPath "${parsedZipPath}"`,
         ]);
 
         compressArchive.on('exit', (code: number, signal: string) => {
             switch (code) {
-            case 0: {
-                resolve(parsedZipPath);
-                return;
-            }
-            default: {
-                reject(new Error(signal));
-            }
+                case 0: {
+                    resolve(parsedZipPath);
+                    return;
+                }
+                default: {
+                    reject(new Error(signal));
+                }
             }
         });
         compressArchive.on('error', (error: Error) => {
@@ -90,5 +92,9 @@ export const dependableCompressZipFileUnix = (folderPath: string, zipPath: strin
     });
 
 export const dependableCompressZipFile = async (folderPath: string, zipPath: string): Promise<string> => {
-    return await dependableCompressZipFileUnix(folderPath, zipPath);
-}; 
+
+    switch (Os.platform()) {
+        case 'win32': return await dependableCompressZipFileWindows(folderPath, zipPath);
+        default: return await dependableCompressZipFileUnix(folderPath, zipPath);
+    }
+};
