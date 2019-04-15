fcdcacbe6a83854899b2038460f2464f330bd4b0
WMXPY <wm@mengw.io>
2019-04-15 00:23:28 -0500

    add hash unit tests

---
+++ test/unit/hash.test.ts
@@ -0,0 +1,33 @@
+/**
+ * @author WMXPY
+ * @namespace IO
+ * @description File
+ * @override
+ */
+
+import { expect } from "chai";
+import * as Chance from "chance";
+import { md5String } from "../../src/hash";
+
+describe('Given [io-hash] helper methods', (): void => {
+
+    const chance: Chance.Chance = new Chance('io-hash');
+
+    it('should be able to md5 string', async (): Promise<void> => {
+
+        const thing: string = chance.string();
+
+        const result1: string = md5String(thing);
+        const result2: string = md5String(thing);
+
+        expect(result1).to.be.equal(result2);
+    });
+
+    it('should be able to return different md5 string', async (): Promise<void> => {
+
+        const result1: string = md5String(chance.string());
+        const result2: string = md5String(chance.string());
+
+        expect(result1).to.not.be.equal(result2);
+    });
+});
