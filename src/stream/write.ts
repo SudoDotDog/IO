1882865bb038e018ee715a052fa45354def4464a
WMXPY <wm@mengw.io>
2020-03-08 15:15:26 -0500

    add stream write function

--- src/stream/write.ts
+++ src/stream/write.ts
@@ -4,14 +4,41 @@
  * @description Write
  */
 
+import * as Fs from "fs";
+
 export class FileWritingStream {
 
-    public static create(): FileWritingStream {
+    public static create(target: string): FileWritingStream {
 
-        return new FileWritingStream();
+        return new FileWritingStream(target);
     }
 
-    private constructor() {
+    private readonly _stream: Fs.WriteStream;
 
+    private constructor(target: string) {
+
+        this._stream = Fs.createWriteStream(target, {
+            flags: 'w',
+            encoding: 'utf8',
+        });
+    }
+
+    public dispose(): this {
+
+        this._stream.close();
+        return this;
+    }
+
+    public write(text: string): Promise<void> {
+
+        return new Promise<void>((resolve: () => void, reject: (reason: any) => void) => {
+            this._stream.write(text, (error?: Error) => {
+
+                if (error) {
+                    reject(error);
+                }
+                resolve();
+            });
+        });
     }
 }
